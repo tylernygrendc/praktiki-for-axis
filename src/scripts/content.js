35527b47724b6e43270e36dea99467c096168a50
Tyler Nygren <tylernygren@gmail.com>
2025-10-21 16:09:53 -0400

    fix imports

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,62 +1,53 @@
-import "web-components.js";
+import "./_components.mjs";
 import { User } from "./library/axis-user";
+import { coerce } from "./_type.mjs";
 
-chrome.runtime.onMessage.addListener((message, sender, response) => {
-    switch(message.source.event){
-        case "action-icon-click":
-            connectPopup();
-        case "sync-login":
-            try{
-                fetch("https://axis.thejoint.com/rest/v11_24/oauth2/token", {
-                    "method": "POST",
-                    "body": {
-                        "username": message.body.username,
-                        "password": message.body.password,
-                        "client_id": "sugar",
-                        "platform": "base",
-                        "client_secret": "",
-                        "current_language": "en_us",
-                        "client_info": { "current_language": "en_us" }
+const user = new User();
+
+switch(user.currentApp.resource){
+    case "login":
+        if(user.currentApp.isBackOffice){
+            coerce.element(document.querySelector("button[type=submit]")).addEventListener("click", async () => {
+                chrome.runtime.sendMessage(
+                    {
+                        source: {
+                            event: "back-office-login",
+                            type: "content-script",
+                            file: "content.js"
+                        },
+                        body: {
+                            username: (() => {
+                                coerce.element(document.querySelector("input#user_name")).value
+                            })(),
+                            password: (() => {
+                                coerce.element(document.querySelector("input#password")).value
+                            })()
+                        }
+                    }, (res) => {
+                        sessionStorage.setItem("frontOfficeAuth", res);
                     }
-                }).then(async res => {
-                    // ! redirect may be necessary
-                    if(res.ok) {
-                        res = await res.json();
-                        setInterval(() => {
-                            // ! assess refresh to avoid storing password
-                        }, await res.json().then((json) => {
-                            return json.expires_in;
-                        }));
-                    }
-                    response(res);
-                });
-            } catch (error) {
-                console.error(error);
-            }
-    }
-});
-
-window.onload = async () => {
-    const user = new User();
-    if(user.isLoggedIn()) await connectExtensionUI();
-    else if(user.currentApp.isBackOffice && user.settings.connectFrontOffice) user.onLogin = () => {
-        chrome.runtime.sendMessage(
-            {
-                source: {
-                    event: "back-office-login",
-                    type: "content-script",
-                    file: "content.js"
-                },
-                body: {
-                    username: user.getUsername(),
-                    password: user.getPassword()
-                }
-            }, (res) => {
-                connectToast(res.ok ? "" : "");
-            }
-        );
-        connectExtensionUI();
-    }
-
+                );
+            });
+        } else if (user.currentApp.isFrontOffice){
+            // do nothing
+        }
+        break;
+    case "cert-create":
+        connectVisitUI(await user.initialize());
+        break;
+    case "home":
+    case "contacts":
+    case "waiting-queue":
+    case "pending-notes":
+    case "completed-visits":
+        connectContactsUI(await user.initialize());
+        break;
+    case "patient-search":
+    case "task-management":
+    case "tasks":
+    case "tj_clinics":
+    case "tj_custom_reports":
+    default:
+        // do nothing
+        break;
 }
-
